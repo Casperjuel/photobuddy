42ecb5ebb8bdaf3f8e5add0511a0e902e494cb81
Casper Juel <casper@adapt.dk>
2018-08-10 22:10:52 +0200

    server test

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 //Requires
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const chalk = require('chalk');
-const morgan = require('morgan');
+const path = require("path");
+const chalk = require("chalk");
+const morgan = require("morgan");
 //Static Routes
-app.use('/static', express.static(path.join(__dirname, 'build/static')));
-app.use(morgan('dev')) // logging
+app.use("/static", express.static(path.join(__dirname, "build/static")));
+app.use(morgan("dev")); // logging
 //Main App Route
-app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, 'build/index.html')));
-const port = 1337;
+app.get("/", (req, res, next) =>
+  res.sendFile(path.join(__dirname, "build/index.html"))
+);
+const port = 80;
 //Run Server
-app.listen(process.env.PORT || port, () => console.log(chalk.blue(`Listening intently on port ${port}`)));
+
+app.listen(port, "photo.buddy");
