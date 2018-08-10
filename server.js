0669773f2a55a6758cdbdc8ecff9e7cb9c4a429c
Casper Juel <casper@adapt.dk>
2018-08-10 21:11:48 +0200

    updated server

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const chalk = require('chalk');
 const morgan = require('morgan');
 //Static Routes
-app.use('/build', express.static(path.join(__dirname, 'build')));
+app.use('/static', express.static(path.join(__dirname, 'build/static')));
 app.use(morgan('dev')) // logging
 //Main App Route
-app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, 'build/index.html')));
 const port = 1337;
 //Run Server
 app.listen(process.env.PORT || port, () => console.log(chalk.blue(`Listening intently on port ${port}`)));
