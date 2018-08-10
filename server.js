14088ec468d8aa4b420c75959e9140b1aa64aef9
Casper Juel <casper@adapt.dk>
2018-08-10 23:26:26 +0200

    testing

--- server.js
+++ server.js
@@ -2,16 +2,52 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const chalk = require("chalk");
-const morgan = require("morgan");
+const bodyParser = require("body-parser");
+
+var fs = require("fs");
+var gphoto2 = require("gphoto2");
+var GPhoto = new gphoto2.GPhoto2();
+
+// Negative value or undefined will disable logging, levels 0-4 enable it.
+GPhoto.setLogLevel(1);
+GPhoto.on("log", function(level, domain, message) {
+  console.log(domain, message);
+});
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 //Static Routes
 app.use("/static", express.static(path.join(__dirname, "build/static")));
-app.use(morgan("dev")); // logging
+
 //Main App Route
 app.get("/", (req, res, next) =>
   res.sendFile(path.join(__dirname, "build/index.html"))
 );
-const port = 80;
+
+app.post("/test", function(req, res) {
+    GPhoto.list(list => {
+        if (list.length === 0) return 'no camera';
+        const picturePath = path.join(__dirname + "build/picture.jpg");
+        const camera = list[0];
+        console.log("Found", camera.model);
+      
+        camera.takePicture({targetPath: "/tmp/foo.XXXXXX"}, (er, tmpname) => {
+            fs.renameSync(tmpname, picturePath);
+          }
+        ).then(function(result) {
+            console.log("Promise worked");
+            res.end(result);
+          }, function(err) {
+            console.log("Something broke");
+            res.send(err);
+          });
+      });
+});
+
+const port = 8000;
+
 //Run Server
 
 app.listen(port);
--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import axios from "axios";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { photo: "" };
+  }
+
+  startBracketing = () => {
+    console.log("bracketing");
+    axios.post('/test')
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+    this.setState({ photo: "https://source.unsplash.com/random" });
+  };
+
+  startFocus = () => {
+    console.log("focus");
+  };
   render() {
-    const startBracketing = () => {
-      console.log("bracketing");
-    };
-
-    const startFocus = () => {
-      console.log("focus");
-    };
-
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          <button onClick={startBracketing}>Trigger bracketing</button>
-          <button onClick={startFocus}>Trigger focus</button>
+          <button onClick={this.startBracketing}>Trigger bracketing</button>
+          <button onClick={this.startFocus}>Trigger focus</button>
+          {this.state.photo && <img src={this.state.photo} alt="picture2" />}
         </p>
       </div>
     );
