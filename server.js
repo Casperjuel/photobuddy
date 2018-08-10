3626ae3e26d9486454089a41d4556694f8f623cc
Casper Juel <casper@adapt.dk>
2018-08-10 23:38:09 +0200

    fixing

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ GPhoto.on("log", function(level, domain, message) {
   console.log(domain, message);
 });
 
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -25,25 +24,38 @@ app.use("/static", express.static(path.join(__dirname, "build/static")));
 app.get("/", (req, res, next) =>
   res.sendFile(path.join(__dirname, "build/index.html"))
 );
-
-app.post("/test", function(req, res) {
+function initialize() {
+  return new Promise(function(resolve, reject) {
     GPhoto.list(list => {
-        if (list.length === 0) return 'no camera';
+      if (list.length === 0) reject("no camera");
+      else {
         const picturePath = path.join(__dirname + "build/picture.jpg");
         const camera = list[0];
         console.log("Found", camera.model);
-      
-        camera.takePicture({targetPath: "/tmp/foo.XXXXXX"}, (er, tmpname) => {
-            fs.renameSync(tmpname, picturePath);
-          }
-        ).then(function(result) {
-            console.log("Promise worked");
-            res.end(result);
-          }, function(err) {
-            console.log("Something broke");
-            res.send(err);
-          });
-      });
+  
+        camera.takePicture({ targetPath: "/tmp/foo.XXXXXX" }, (er, tmpname) => {
+          fs.renameSync(tmpname, picturePath);
+        });
+        resolve(picturePath);
+          
+      }
+
+    });
+  });
+}
+
+app.post("/test", function(req, res) {
+  var initializePromise = initialize();
+  initializePromise.then(
+    function(result) {
+      console.log("Promise worked");
+      res.end(result);
+    },
+    function(err) {
+      console.log("Something broke");
+      res.send(err);
+    }
+  );
 });
 
 const port = 8000;
